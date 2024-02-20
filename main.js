26417c78e215f6bf5f0924319c062152f1cdc74c
Chalel <charlie03.pl@gmail.com>
2024-02-20 14:31:58 +0100

    Skapat färg fil

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-let suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
 
 class Card {
     constructor(suit, value) {
         this.suit = suit;
         this.value = value;
+        this.color = this.determineColor(suit);
+    }
+
+    determineColor(suit) {
+        if (suit === 'Hearts' || suit === 'Diamonds') {
+            return 'Red';
+        } else {
+            return 'Black';
+        }
     }
 }
 
@@ -21,8 +28,30 @@ class Deck {
         }
         return this.deck;
     }
-}
+    shuffle() {
+        let counter = this.deck.length, temp, i;
 
-let deck = new deck();
+        while (counter) {
+            i = Math.floor(Math.random() * counter--);
+            temp = this.deck[counter];
+            this.deck[counter] = this.deck[i];
+            this.deck[i] = temp;
+        }
+        return this.deck;
+    }
+    deal() {
+        let hand = [];
+        while (hand.length < 2) {
+            hand.push(this.deck.pop());
+        }
+        return hand;
+    }
+}
+let suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+let deck = new Deck();
+
 deck.createDeck(suits, values);
-console.log(deck);
+// console.log(deck.deal());
+console.log(deck.shuffle());
+console.log(deck.deal());
